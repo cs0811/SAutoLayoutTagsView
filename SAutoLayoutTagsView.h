c9d9902c3d88b5c1d1782a334da421f53235ccdb
tongxuan <tong_xuan@hunliji.com>
2016-11-23 10:27:03 +0800

    update

--- SAutoLayoutTagsView.h
+++ SAutoLayoutTagsView.h
@@ -8,16 +8,70 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SAutoLayoutTagsView : UIView
+@interface TagItem : UIButton
+/**
+ 每个标签高   (默认 30.)
+ */
+@property (nonatomic, assign) CGFloat itemHeight;
+/**
+ 每个标签宽 （适用于等宽标签）   (默认 60.)
+ */
+@property (nonatomic, assign) CGFloat itemWidth;
+/**
+ 标签内左边距   (默认 6.)
+ */
+@property (nonatomic, assign) CGFloat itemInnerLeftSpace;
+/**
+ 标签内右边距   (默认 6.)
+ */
+@property (nonatomic, assign) CGFloat itemInnerRightSpace;
+/**
+ 标签选中背景颜色
+ */
+@property (nonatomic, strong) UIColor * tagSelectedBGColor;
+/**
+ 标签未选中背景颜色
+ */
+@property (nonatomic, strong) UIColor * tagUnSelectedBGColor;
+/**
+ 标签选中标签颜色
+ */
+@property (nonatomic, strong) UIColor * tagSelectedTitleColor;
+/**
+ 标签未选中标签颜色
+ */
+@property (nonatomic, strong) UIColor * tagUnSelectedTitleColor;
+/**
+ 标签选中圆角
+ */
+@property (nonatomic, assign) CGFloat tagSelectedCornerRadius;
+/**
+ 标签未选中圆角
+ */
+@property (nonatomic, assign) CGFloat tagUnSelectedCornerRadius;
+/**
+ 标签选中边框宽
+ */
+@property (nonatomic, assign) CGFloat tagSelectedBorderWidth;
+/**
+ 标签未选中边框宽
+ */
+@property (nonatomic, assign) CGFloat tagUnSelectedBorderWidth;
+/**
+ 标签选中边框颜色
+ */
+@property (nonatomic, strong) UIColor * tagSelectedBorderColor;
+/**
+ 标签未选中边框颜色
+ */
+@property (nonatomic, strong) UIColor * tagUnSelectedBorderColor;
+@property (nonatomic, strong) UIFont * tagSelectedFont;
+@property (nonatomic, strong) UIFont * tagUnSelectedFont;
+@property (nonatomic, strong) UIColor * tagHilightTitleColor;
+@end
 
-/*
- *  每个标签高   (默认 30.)
- */
-@property (nonatomic, assign) CGFloat height;
-/*
- *  每个标签宽   (默认 30.)
- */
-@property (nonatomic, assign) CGFloat width;
+
+@interface LightLuxuryTagsView : UIView
 /*
  *  左边距     (默认 12.)
  */
@@ -49,9 +103,9 @@
 @property (nonatomic, assign) BOOL resetAllTagWhenSingleClick;
 
 /*
- *  标签初始状态
+ *  已选中的标签,初始时传入
  */
-@property (nonatomic, copy) NSArray * tagsStartStatusArr;
+@property (nonatomic, copy) NSArray * selectedTagsArr;
 
 /*
  *  总行数
@@ -62,12 +116,14 @@
  */
 @property (nonatomic, assign, readonly) CGFloat totalHeight;
 
+/**
+ 标签能否被点击
+ */
 @property (nonatomic, assign) BOOL tagShouldClick;
 
-@property (nonatomic, strong) UIColor * tagSelectedColor;
-
-@property (nonatomic, strong) UIColor * tagUnSelectedColor;
-
+/**
+ 标签是否等宽
+ */
 @property (nonatomic, assign) BOOL shouldEqualItemWitdh;
 
 /*
@@ -75,16 +131,24 @@
  */
 @property (nonatomic, assign) CGFloat maxWidth;
 
-
 /*
  *  最后选择的tag下标
  */
 @property (nonatomic, strong, readonly) NSArray * tagsIndexArr;
 
+@property (nonatomic, strong) TagItem * item;
+
+@property (nonatomic, copy) NSString * tagHilightTitle;
+
+/**
+ 限制每个标签字数
+ */
+@property (nonatomic, assign) NSInteger maxTagTextlength;
 
 @property (nonatomic, copy) void(^tagClick)(UIButton *tagBtn, NSInteger index);
 @property (nonatomic, copy) void(^tagsArrClick)(NSArray * indexArr);
 
 
 - (void)showTagsWithDataArr:(NSArray *)arr onView:(UIView *)view;
+
 @end
--- SAutoLayoutTagsView.m
+++ SAutoLayoutTagsView.m
@@ -11,7 +11,61 @@
 #define kScreenW        [UIScreen mainScreen].bounds.size.width
 #define kTagBase        1024
 
-@implementation SAutoLayoutTagsView
+@implementation TagItem
+- (CGFloat)itemWidth {
+    return _itemWidth?:60.;
+}
+- (CGFloat)itemHeight {
+    return _itemHeight?:30.;
+}
+- (CGFloat)itemInnerLeftSpace {
+    return _itemInnerLeftSpace<0?0:_itemInnerLeftSpace;
+}
+- (CGFloat)itemInnerRightSpace {
+    return _itemInnerRightSpace<0?0:_itemInnerRightSpace;
+}
+- (UIColor *)tagSelectedBGColor {
+    return _tagSelectedBGColor?:[UIColor magentaColor];
+}
+- (UIColor *)tagUnSelectedBGColor {
+    return _tagUnSelectedBGColor?:[UIColor whiteColor];
+}
+- (UIColor *)tagSelectedTitleColor {
+    return _tagSelectedTitleColor?:[UIColor blackColor];
+}
+- (UIColor *)tagUnSelectedTitleColor {
+    return _tagUnSelectedTitleColor?:[UIColor lightGrayColor];
+}
+- (CGFloat)tagSelectedCornerRadius {
+    return _tagSelectedCornerRadius<0?0:_tagSelectedCornerRadius;
+}
+- (CGFloat)tagUnSelectedCornerRadius {
+    return _tagUnSelectedCornerRadius<0?0:_tagUnSelectedCornerRadius;
+}
+- (CGFloat)tagSelectedBorderWidth {
+    return _tagSelectedBorderWidth<0?0:_tagSelectedBorderWidth;
+}
+- (CGFloat)tagUnSelectedBorderWidth {
+    return _tagUnSelectedBorderWidth<0?0:_tagUnSelectedBorderWidth;
+}
+- (UIColor *)tagSelectedBorderColor {
+    return _tagSelectedBorderColor?:[UIColor whiteColor];
+}
+- (UIColor *)tagUnSelectedBorderColor {
+    return _tagUnSelectedBorderColor?:[UIColor lightGrayColor];
+}
+- (UIFont *)tagSelectedFont {
+    return _tagSelectedFont?:[UIFont systemFontOfSize:14.];
+}
+- (UIFont *)tagUnSelectedFont {
+    return _tagUnSelectedFont?:[UIFont systemFontOfSize:14.];
+}
+- (UIColor *)tagHilightTitleColor {
+    return _tagHilightTitleColor?:[UIColor whiteColor];
+}
+@end
+
+@implementation LightLuxuryTagsView
 {
     NSArray * _dataArr;
     NSMutableArray * _tagsArr;
@@ -36,30 +90,28 @@
 
 - (void)removeTags {
     for (UIView * view in self.subviews) {
-        if ([view isKindOfClass:[UIButton class]]) {
+        if ([view isKindOfClass:[TagItem class]]) {
             [view removeFromSuperview];
         }
     }
 }
-
 - (void)sizeBtn:(NSArray *)arr{
-    
-    //    CGFloat width = 60.;
-    CGFloat height = _height?:30.;
-    CGFloat leftSpace = _leftSpace?:12;             // 左边距
-    CGFloat rightSpace = _rightSpace?:12;           // 右边距
-    CGFloat topSpace = _topSpace?:5;                // 上边距
-    CGFloat bottomSpace = _bottomSpace?:5;          // 下边距
-    CGFloat itemSpaceH = _itemSpaceH?:10;           // 水平间距
-    CGFloat itemSpaceV = _itemSpaceV?:5;            // 竖直间距
-    
-    if (!_tagSelectedColor) {
-        _tagSelectedColor = [UIColor cyanColor];
+    if (!_item) {
+        NSLog(@"tagsView -- item is nil");
+        return;
     }
     
-    if (!_tagUnSelectedColor) {
-        _tagUnSelectedColor = [UIColor whiteColor];
-    }
+    CGFloat width = _item.itemWidth;
+    CGFloat height = _item.itemHeight;
+    CGFloat leftSpace = _leftSpace>=0?_leftSpace:12;             // 左边距
+    CGFloat rightSpace = _rightSpace>=0?_rightSpace:12;           // 右边距
+    CGFloat topSpace = _topSpace>=0?_topSpace:5;                // 上边距
+    CGFloat bottomSpace = _bottomSpace>=0?_bottomSpace:5;          // 下边距
+    CGFloat itemSpaceH = _itemSpaceH>=0?_itemSpaceH:10;           // 水平间距
+    CGFloat itemSpaceV = _itemSpaceV>=0?_itemSpaceV:5;            // 竖直间距
+    CGFloat itemInnerLeftSpac = _item.itemInnerLeftSpace;
+    CGFloat itemInnerRightSpace = _item.itemInnerRightSpace;
+    NSString * hilightTitle = _tagHilightTitle?:@"";
     
     if (_maxWidth<=0) {
         _maxWidth = kScreenW;
@@ -67,44 +119,61 @@
     
     //    循环创建数组
     CGFloat validitySpace = _maxWidth-leftSpace-rightSpace;      // 可用的宽度
-    UIButton * lastBtn = nil;          // 上一个宽度
+    TagItem * lastBtn = nil;          // 上一个宽度
     
     for (int i = 0; i < arr.count; i++) {
-        UIButton * sizeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        TagItem * sizeBtn = [TagItem buttonWithType:UIButtonTypeCustom];
         sizeBtn.userInteractionEnabled = _tagShouldClick;
-        [sizeBtn setBackgroundColor:_tagUnSelectedColor];
-        sizeBtn.layer.borderWidth = 1;
-        sizeBtn.layer.borderColor = kColorLine1.CGColor;
-        sizeBtn.layer.cornerRadius = height/2;
+        [sizeBtn setBackgroundColor:_item.tagUnSelectedBGColor];
+        sizeBtn.layer.borderWidth = _item.tagUnSelectedBorderWidth;
+        sizeBtn.layer.borderColor = _item.tagUnSelectedBorderColor.CGColor;
+        sizeBtn.layer.cornerRadius = _item.tagUnSelectedCornerRadius;
         sizeBtn.layer.masksToBounds = YES;
-        [sizeBtn setTitleColor:kColorGray1 forState:UIControlStateNormal];
-        sizeBtn.titleLabel.font = [UIFont defaultFontWithSize:14.0];
+        [sizeBtn setTitleColor:_item.tagUnSelectedTitleColor forState:UIControlStateNormal];
+        sizeBtn.titleLabel.font = _item.tagUnSelectedFont;
         sizeBtn.tag = kTagBase+i;
         sizeBtn.selected = NO;
+        NSString * title = arr[i];
+        // 限制字数
+        if (self.maxTagTextlength > 0) {
+            if (title.length > self.maxTagTextlength) {
+                title = [title substringToIndex:self.maxTagTextlength];
+            }
+        }
+        [sizeBtn setTitle:title forState:UIControlStateNormal];
+        [self addSubview:sizeBtn];
         
-        for (NSString * value in self.tagsStartStatusArr) {
-            if ([value isEqual:arr[i]]) {
+        for (NSString * value in self.selectedTagsArr) {
+            if ([value isEqual:title]) {
                 sizeBtn.selected = YES;
-                sizeBtn.backgroundColor = _tagSelectedColor;
-                sizeBtn.layer.borderWidth = 0;
-                [sizeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                sizeBtn.backgroundColor = _item.tagSelectedBGColor;
+                sizeBtn.layer.borderWidth = _item.tagSelectedBorderWidth;
+                sizeBtn.layer.cornerRadius = _item.tagSelectedCornerRadius;
+                sizeBtn.layer.borderColor = _item.tagSelectedBorderColor.CGColor;
+                [sizeBtn setTitleColor:_item.tagSelectedTitleColor forState:UIControlStateNormal];
+                sizeBtn.titleLabel.font = _item.tagSelectedFont;
                 [_tagsArr addObject:@(i)];
             }
         }
         
-        [sizeBtn setTitle:arr[i] forState:UIControlStateNormal];
-        [self addSubview:sizeBtn];
+        // 高亮处理
+        NSRange hilightRange = [title rangeOfString:hilightTitle];
+        if (self.tagHilightTitle && self.tagHilightTitle.length>0 && hilightRange.location != NSNotFound) {
+            NSMutableAttributedString * hilightAttribute = [[NSMutableAttributedString alloc] initWithString:title];
+            [hilightAttribute addAttribute:NSForegroundColorAttributeName value:_item.tagHilightTitleColor range:hilightRange];
+            [sizeBtn setAttributedTitle:hilightAttribute forState:UIControlStateNormal];
+        }
         
         [sizeBtn addTarget:self action:@selector(chooseTag:) forControlEvents:UIControlEventTouchUpInside];
         
-        CGSize size = [arr[i] boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, height) options:NSStringDrawingUsesFontLeading | NSStringDrawingTruncatesLastVisibleLine attributes:@{NSFontAttributeName:[UIFont defaultFontWithSize:12.0]} context:nil].size;
-        size = CGSizeMake(size.width+12, size.height);
-        CGRect frame = CGRectZero;
-        
-        
-        if (_shouldEqualItemWitdh) {
-            size = CGSizeMake(_width?:30, size.height);
+        CGSize size = [title boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, height) options:NSStringDrawingUsesFontLeading | NSStringDrawingTruncatesLastVisibleLine attributes:@{NSFontAttributeName:_item.tagUnSelectedFont} context:nil].size;
+        if (self.shouldEqualItemWitdh) {
+            // 等宽
+            size = CGSizeMake(width+itemInnerLeftSpac+itemInnerRightSpace, size.height);
+        }else {
+            size = CGSizeMake(size.width+itemInnerLeftSpac+itemInnerRightSpace, size.height);
         }
+        CGRect frame = CGRectZero;
         
         validitySpace = _maxWidth-CGRectGetMaxX(lastBtn.frame)-rightSpace-itemSpaceH;
         
@@ -135,11 +204,11 @@
 }
 
 #pragma mark action
-- (void)chooseTag:(UIButton *)sender {
+- (void)chooseTag:(TagItem *)sender {
     if (_resetAllTagWhenSingleClick) {
         // 重置
         for (int i = 0; i<_dataArr.count; i++) {
-            UIButton * btn = [self viewWithTag:kTagBase+i];
+            TagItem * btn = [self viewWithTag:kTagBase+i];
             if (sender == btn) {
                 [self setBtnSelectedStatus:btn];
                 [_tagsArr removeAllObjects];
@@ -173,18 +242,24 @@
     return _tagsArr;
 }
 
-- (void)setBtnSelectedStatus:(UIButton *)btn {
+- (void)setBtnSelectedStatus:(TagItem *)btn {
     btn.selected = YES;
-    btn.backgroundColor = _tagSelectedColor;
-    btn.layer.borderWidth = 0;
-    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    btn.layer.cornerRadius = _item.tagSelectedCornerRadius;
+    btn.layer.borderColor = _item.tagSelectedBorderColor.CGColor;
+    btn.titleLabel.font = _item.tagSelectedFont;
+    btn.backgroundColor = _item.tagSelectedBGColor;
+    btn.layer.borderWidth = _item.tagSelectedBorderWidth;
+    [btn setTitleColor:_item.tagSelectedTitleColor forState:UIControlStateNormal];
 }
 
-- (void)setBtnUnSelectedStatus:(UIButton *)btn {
+- (void)setBtnUnSelectedStatus:(TagItem *)btn {
     btn.selected = NO;
-    btn.backgroundColor = _tagUnSelectedColor;
-    btn.layer.borderWidth = 1;
-    [btn setTitleColor:kColorGray1 forState:UIControlStateNormal];
+    btn.layer.cornerRadius = _item.tagUnSelectedCornerRadius;
+    btn.layer.borderColor = _item.tagUnSelectedBorderColor.CGColor;
+    btn.titleLabel.font = _item.tagUnSelectedFont;
+    btn.backgroundColor = _item.tagUnSelectedBGColor;
+    btn.layer.borderWidth = _item.tagUnSelectedBorderWidth;
+    [btn setTitleColor:_item.tagUnSelectedTitleColor forState:UIControlStateNormal];
 }
 
 @end
